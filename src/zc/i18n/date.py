f875d51f88859ea498736ac1a9e8ad7ea9824635
Bernd Dorn <bernddorn@gmail.com>
2006-04-30 14:04:17 +0000

    fixed handling of daylight savings in date.normalize, this should actually be fixed in pytz

--- src/zc/i18n/date.py
+++ src/zc/i18n/date.py
@@ -31,8 +31,45 @@ def format(request, dt=None):
         'dateTime', 'medium')
     return formatter.format(dt)
 
-
 def normalize(request, dt):
+
+    """this method normalizes datetime instances by converting them to
+    utc, daylight saving times are also taken into account. This
+    method requires an adapter to get the tzinfo from the request.
+
+    >>> from zope import component, interface
+    >>> import pytz
+    >>> from zope.interface.common.idatetime import ITZInfo
+    >>> from zope.publisher.interfaces.browser import IBrowserRequest
+    >>> from zope.publisher.browser import TestRequest
+    >>> @interface.implementer(ITZInfo)
+    ... @component.adapter(IBrowserRequest)
+    ... def tzinfo(request):
+    ...     return pytz.timezone('Europe/Vienna')
+    >>> component.provideAdapter(tzinfo)
+    >>> dt = datetime.datetime(2006,5,1,12)
+    >>> request = TestRequest()
+
+    The Vienna timezone has a 2 hour offset to utc at this date.
+    >>> normalize(request,dt)
+    datetime.datetime(2006, 5, 1, 10, 0, tzinfo=<UTC>)
+
+    At this date the timezone has only a one hour offset.
+    >>> dt = datetime.datetime(2006,2,1,12)    
+    >>> normalize(request,dt)
+    datetime.datetime(2006, 2, 1, 11, 0, tzinfo=<UTC>)
+    """
+
+    
     if dt.tzinfo is None:
-        dt = dt.replace(tzinfo=ITZInfo(request))
+        tzinfo = ITZInfo(request)
+    else:
+        tzinfo = dt.tzinfo
+        
+    # we have to do this because, pytz does currently not take the
+    # datetime argument into account in its dst() and utcoffset()
+    # methods
+    
+    tzu = tzinfo.fromutc(dt).tzinfo
+    dt = dt.replace(tzinfo=tzu)
     return dt.astimezone(pytz.utc)
--- src/zc/i18n/tests.py
+++ src/zc/i18n/tests.py
@@ -4,6 +4,7 @@ from zope.testing import doctest
 def test_suite():
     return unittest.TestSuite((
         doctest.DocFileSuite('duration.txt'),
+        doctest.DocTestSuite('zc.i18n.date')
         ))
 
 if __name__ == '__main__':
