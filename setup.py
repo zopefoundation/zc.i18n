b19612e5ea21f70bb66c60abc003e350f25fc28a
Martijn Faassen <faassen@startifact.com>
2006-05-24 15:53:58 +0000

    eggify this package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zc.i18n",
+    version="0.5",
+    packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
+    
+    package_dir= {'':'src'},
+    
+    namespace_packages=['zc'],
+    package_data = {
+    '': ['*.txt', '*.zcml'],
+    },
+
+    zip_safe=False,
+    author='Zope Project',
+    author_email='zope3-dev@zope.org',
+    description="""\
+zc.i18n contains a i18n related helper code.
+""",
+    license='ZPL',
+    keywords="zope zope3",
+    )
