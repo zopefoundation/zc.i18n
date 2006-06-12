b5af89b40316ac7420fbffd696952dc9f70fc7a2
Brian Sutherland <brian@vanguardistas.net>
2006-06-12 22:16:31 +0000

    Add missing copyright headers and include the ZPL.

--- src/zc/i18n/duration.py
+++ src/zc/i18n/duration.py
@@ -1,3 +1,16 @@
+#############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from zope import i18n
 from zc.i18n.i18n import _
 
