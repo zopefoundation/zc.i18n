2d8396c284b6eaa72020437fb68ee065c6427992
Stephan Richter <stephan.richter@gmail.com>
2007-11-03 06:22:18 +0000

    Fix package data.

---
+++ README.txt
@@ -0,0 +1,2 @@
+This package provides additional I18n and L10n features. In particular it
+provides an API to compute the time duratrions over various timezones.
--- setup.py
+++ setup.py
@@ -1,23 +1,62 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for z3c.viewlet package
+
+$Id: setup.py 81038 2007-10-24 14:34:17Z srichter $
+"""
+import os
 from setuptools import setup, find_packages
 
-setup(
-    name="zc.i18n",
-    version="0.6.1dev",
-    packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
-    
-    package_dir= {'':'src'},
-    
-    namespace_packages=['zc'],
-    package_data = {
-    '': ['*.txt', '*.zcml'],
-    },
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-    zip_safe=False,
-    author='Zope Project',
-    author_email='zope3-dev@zope.org',
-    description="""\
-zc.i18n contains a i18n related helper code.
-""",
-    license='ZPL',
-    keywords="zope zope3",
-    )
+setup(name='zc.i18n',
+      version = '0.5.2',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      description='Additional I18n Support APIs',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          'Detailed Dcoumentation\n' +
+          '======================\n'
+          + '\n\n' +
+          read('src', 'zc', 'i18n', 'duration.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 i18n date time duration",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zc.i18n',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zc'],
+      extras_require=dict(test=['zope.testing']),
+      install_requires=['setuptools',
+                        'zope.i18n',
+                        'zope.i18nmessageid',
+                        'zope.interface'],
+      include_package_data = True,
+      zip_safe = False,
+      )
