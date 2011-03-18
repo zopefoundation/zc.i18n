2743696c79ce9f66fae51ef532cedb807e532591
Michael Howitz <mh@gocept.com>
2011-03-18 07:44:41 +0000

    Using Python's ``doctest`` module instead of depreacted ``zope.testing.doctest``.

--- src/zc/i18n/tests.py
+++ src/zc/i18n/tests.py
@@ -12,13 +12,10 @@
 #
 ##############################################################################
 import unittest
-from zope.testing import doctest
+import doctest
 
 def test_suite():
     return unittest.TestSuite((
         doctest.DocFileSuite('duration.txt'),
         doctest.DocTestSuite('zc.i18n.date')
         ))
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
